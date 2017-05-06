e3c4a293b4899c29896c90ab436dfa18af9263bc
damifil <damifil15@gmail.com>
2017-05-06 23:17:42 +0200

    dodanie testowego klienta konsolowego i zmiana w serwerze
    do serwera dodane obsluga wielowatkowych socketow TCP (klientow)

--- TIPySerwer/TIPySerwer/Program.cs
+++ TIPySerwer/TIPySerwer/Program.cs
@@ -2,23 +2,110 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Security.Cryptography;
 using System.Security.Policy;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using TIPySerwer.Models;
 
 namespace TIPySerwer
 {
+
+
+    class TcpServer
+    {
+        private TcpListener _server;
+        private Boolean _isRunning;
+        private DiffieHelman diffieHelman;
+        public TcpServer(int port)
+        {
+            _server = new TcpListener(IPAddress.Any, port);
+            _server.Start();
+
+            _isRunning = true;
+
+            LoopClients();
+        }
+
+        public void LoopClients()
+        {
+            while (_isRunning)
+            {
+                // wait for client connection
+                TcpClient newClient = _server.AcceptTcpClient();
+
+                // client found.
+                // create a thread to handle communication
+                Thread t = new Thread(new ParameterizedThreadStart(HandleClient));
+                t.Start(newClient);
+            }
+        }
+
+        public void HandleClient(object obj)
+        {
+            Random rnd = new Random();
+            TcpClient client = (TcpClient)obj;
+            diffieHelman = new DiffieHelman();
+            StreamWriter sWriter = new StreamWriter(client.GetStream(), Encoding.ASCII);
+            StreamReader sReader = new StreamReader(client.GetStream(), Encoding.ASCII);
+            Boolean bClientConnected = true;
+            String sData = null;
+
+              //interesujaca nas obecnie klasa tutaj cala logika wspolpracy serwera z klientem bedzie umiesczona
+            while (bClientConnected)
+            {
+                //nasluchiwanie komunikatu
+                sData = sReader.ReadLine();
+
+                //ify dotyczace komunikatow
+                if (sData == "CREATE")
+                {
+                    //utworzenie DH
+                    diffieHelman.createDH(diffieHelman, sReader, sWriter);
+                }
+                //if(sData =="LOGIN")
+                //if(sData== "REGISTER")
+                //itd. w zaleznosci od potrzeb
+                if (sData == "send")
+                {
+                    sData = diffieHelman.messageRecive(sReader, diffieHelman);
+                    Console.WriteLine("Otrzymana wiadomosc: " + sData);
+                    Console.WriteLine("Wiadomosc zwrotna: ");
+                    sData = Console.ReadLine();
+
+                    //funnkcja odpowiedzialna za wysylanie do klieenta chwilowo nie przewidzialem by klient wiadomosci odroznial podlug komunikatow
+                    //tylko po prostu je odbiera
+                    diffieHelman.sendMessage(sData, diffieHelman, sWriter);
+                }
+
+                if (sData == "exit")
+                {
+                    bClientConnected = false;
+                }
+            }
+
+        }
+    }
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            
-            
-            UserManager.AddUser("Damian", "haslo").Wait();
-            UserManager.Logging("Damian", "haslo", "192.168.122.123").Wait();
+            //numer portu na ktorym bedzie nasluchiwac i uruchomienie serwera wielowatkowego
+            TcpServer server = new TcpServer(5555);
+
+
+
+            //
+            //UserManager.AddUser("Damian", "haslo").Wait();
+            //UserManager.Logging("Damian", "haslo", "192.168.122.123").Wait();
 
 
             /*
---
+++ testowyKonsolowyKlient/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    
+
+
+class Client
+    {
+        private TcpClient _client;
+
+        private StreamReader _sReader;
+        private StreamWriter _sWriter;
+
+        private Boolean _isConnected;
+        private DiffieHelman diffieHelman;
+        Random rnd = new Random();
+
+        public Client(String ipAddress, int portNum)
+        {
+            _client = new TcpClient();
+            _client.Connect(ipAddress, portNum);
+
+            HandleCommunication();
+            
+            _client.GetStream().Close();
+            _client.Close();
+            _client.Dispose();
+        }
+
+
+
+        public void HandleCommunication()
+        {
+            _sReader = new StreamReader(_client.GetStream(), Encoding.ASCII);
+            _sWriter = new StreamWriter(_client.GetStream(), Encoding.ASCII);
+
+            _isConnected = true;
+            String sData = null;
+            diffieHelman = new DiffieHelman();
+
+
+            //utworzenie polacenia szyfrowanego
+            diffieHelman.CreateDH(_sReader, _sWriter, diffieHelman);
+
+            //komunikacja z serwerem szyfrowana
+            while (_isConnected)
+            {
+                //pomocnicze
+                Console.Write("wartosc do wyslania: ");
+                sData = Console.ReadLine();
+                //wysylanie wiadomosci sData to string ktory ma zostac wyslany (komunikat SEND)
+                diffieHelman.sendMessage(sData, diffieHelman, _sWriter,"send");
+                
+                // nasluchiwanie otrzymywania danych z serwera (po otrzymaniu paczki zwraca stringa
+                String sDataIncomming = diffieHelman.reciveMessage(_sReader, diffieHelman);
+                Console.WriteLine("Otrzymane dane z serwera: "+sDataIncomming);
+
+                //w przypadku gdy klient wysyla exit zamykamy polaczenie (wylaczamy watek
+                if(sData=="exit")
+                {
+                    _isConnected = false;
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Multi-Threaded TCP Server Demo");
+            Console.WriteLine("Provide IP:");
+            String ip = Console.ReadLine();
+
+            Console.WriteLine("Provide Port:");
+            int port = Int32.Parse(Console.ReadLine());
+
+            Client client = new Client(ip, port);
+
+        }
+    }
+}
