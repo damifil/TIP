c09b1fe81e4ffcde4a58914f3a7f9df9f1c9e28e
damifil <damifil15@gmail.com>
2017-05-22 17:04:33 +0200

    dodanie kolejnych dwoch widokow do nawiazywania rozmowy

---
+++ TIP-yKlient/ClientAplication/ClientAplication/CallFrom.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClientAplication
+{
+    /// <summary>
+    /// Interaction logic for CallFrom.xaml
+    /// </summary>
+    public partial class CallFrom : Window
+    {
+        internal User user;
+        public CallFrom()
+        {
+            InitializeComponent();
+        }
+
+
+        bool _shown;
+        protected override void OnContentRendered(EventArgs e)
+        {
+            base.OnContentRendered(e);
+
+            if (_shown)
+                return;
+
+
+            stringWithName.Text = "połączenia z " + user.Name;
+            _shown = true;
+
+        }
+
+
+        private void callDisconectTextboxaction(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void callAceptTextboxaction(object sender, MouseButtonEventArgs e)
+        {
+            CallToWindow main = new CallToWindow();
+            main.user = user;
+            main.Show();
+            this.Close();
+
+        }
+
+
+    }
+}
---
+++ TIP-yKlient/ClientAplication/ClientAplication/CallTransmision.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClientAplication
+{
+    /// <summary>
+    /// Interaction logic for CallTransmision.xaml
+    /// </summary>
+    public partial class CallTransmision : Window
+    {
+        internal User user;
+        public CallTransmision()
+        {
+            InitializeComponent();
+        }
+
+        bool _shown;
+        protected override void OnContentRendered(EventArgs e)
+        {
+            base.OnContentRendered(e);
+
+            if (_shown)
+                return;
+
+
+            stringWithName.Text = "połączenia z " + user.Name;
+            _shown = true;
+
+        }
+
+
+        private void callDisconectTextboxaction(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+
+        }
+
+    }
+}
